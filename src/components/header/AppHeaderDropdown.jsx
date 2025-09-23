ead08b934e18fd3fb4543de635d9db9ddd021daf
IT20264352-Amarasinghe <it20264352@my.sliit.lk>
2025-09-23 21:10:40 +0530

    implement the logout

--- src/components/header/AppHeaderDropdown.jsx
+++ src/components/header/AppHeaderDropdown.jsx
@@ -22,19 +22,27 @@ import {
 } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
 
-import avatar8 from './../../assets/images/avatars/8.jpg'
+import avatar from './../../assets/images/avatars/10.png'
+import { useNavigate } from 'react-router-dom'
 
 const AppHeaderDropdown = () => {
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('token')   // Clear token
+    navigate('/login')                 // Redirect to login page
+  }
+
   return (
     <CDropdown variant="nav-item">
       <CDropdownToggle placement="bottom-end" className="py-0 pe-0" caret={false}>
-        <CAvatar src={avatar8} size="md" />
+        <CAvatar src={avatar} size="md" />
       </CDropdownToggle>
       <CDropdownMenu className="pt-0" placement="bottom-end">
         <CDropdownDivider />
-        <CDropdownItem href="#">
+        <CDropdownItem onClick={handleLogout}>
           <CIcon icon={cilLockLocked} className="me-2" />
-          Lock Account
+          Logout
         </CDropdownItem>
       </CDropdownMenu>
     </CDropdown>
